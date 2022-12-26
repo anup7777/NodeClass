4f08104080c0077950120f19a461e7397885b688
anup7777 <burlakotianup@gmail.com>
2022-12-26 08:55:53 +0545

    env

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ const auth = require('./middleware/auth')
 
 const port = process.env.PORT || 3000
 
-mongoose.connect(process.env.DB_URI).then(() => {
-    console.log('Connected to MongoDB')
-}).catch((err) => console.log(err))
+mongoose.connect("mongodb://localhost:27017/books-29B")
+    .then(() => {
+        console.log("Connected to MongoDB server");
+        app.listen(port, () => {
+            console.log(`App is running on the port: ${port}`);
+        });
+    })
+    .catch((err) => console.log(err));
+
 
 const app = express()
 
@@ -43,8 +49,3 @@ app.use((err, req, res, next) => {
     res.json({ msg: err.message })
 })
 
-mongoose.connection.once('open', () => {
-    app.listen(port, () => {
-        console.log(`Server is running at port ${port}`)
-    })
-})
